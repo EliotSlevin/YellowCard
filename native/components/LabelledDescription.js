3e1fc12e7014bd99552b1be5cfd658a0ea309e83
Chris Chou <chhschou@gmail.com>
2017-12-27 14:06:44 +1300

    convert tab to space

--- native/components/LabelledDescription.js
+++ native/components/LabelledDescription.js
@@ -4,19 +4,19 @@ import { View, Text, StyleSheet } from 'react-native'
 import styles from '../styles'
 
 const componentStyles = StyleSheet.create({
-	container: {
-		marginTop: 5,
-		marginBottom: 5,
-		marginLeft: 15,
-	},
-	label: {
-		textAlign: 'left',
-		...styles.bold16
-	},
-	description: {
-		textAlign: 'left',
-		...styles.regular21
-	},
+  container: {
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 15,
+  },
+  label: {
+    textAlign: 'left',
+    ...styles.bold16
+  },
+  description: {
+    textAlign: 'left',
+    ...styles.regular21
+  },
 })
 
 export default class LabelledDescription extends React.Component {
@@ -24,7 +24,7 @@ export default class LabelledDescription extends React.Component {
     return (
       <View style={componentStyles.container}>
         <Text style={[componentStyles.label]}>{this.props.label}</Text>
-				<Text style={[componentStyles.description]}>{this.props.description}</Text>
+        <Text style={[componentStyles.description]}>{this.props.description}</Text>
       </View>
     )
   }
