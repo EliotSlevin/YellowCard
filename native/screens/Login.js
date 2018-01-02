09e978fffd7036476aa4b42e11a2ccf34c806f77
Chris Chou <chhschou@gmail.com>
2018-01-02 17:15:53 +1300

    add react-native-element ui kit

--- native/screens/Login.js
+++ native/screens/Login.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, TextInput, View, SafeAreaView, Button } from 'react-native';
 import { connect } from 'react-redux'
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 import styles from '../styles'
 import * as colors from '../modules/shared/colors'
 import { initFirebaseRequest } from '../modules/shared'
+
 
 const componentStyles = StyleSheet.create({
   safeArea: {
@@ -36,12 +38,15 @@ export class Login extends Component {
   render() {
     const err = this.props.shared.firebase.err
 
+    // remove securetextentry={true} after update react native/eject
     return (
       <SafeAreaView style={componentStyles.safeArea} forceInset={{ horizontal: 'always', top: 'always' }}>
         <View style={componentStyles.container}>
-          {err && <Text>{err}</Text>}
-          <TextInput placeholder='email' value={this.state.email} onChangeText={(text) => this.setState({ email: text })} />
-          <TextInput placeholder='password' password={true} onChangeText={(text) => this.setState({ password: text })} />
+          <FormValidationMessage>{err}</FormValidationMessage>
+          <FormLabel>Email Address</FormLabel>
+          <FormInput placeholder='Email Address' value={this.state.email} onChangeText={(text) => this.setState({ email: text })} />
+          <FormLabel>Password</FormLabel>
+          <FormInput secureTextEntry={true} password={true} placeholder='Password' onChangeText={(text) => this.setState({ password: text })} />
           <Button title='Sign in' onPress={() => this.onLoginAction()}></Button>
         </View>
       </SafeAreaView>
