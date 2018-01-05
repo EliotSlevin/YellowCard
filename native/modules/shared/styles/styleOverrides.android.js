7b12cd5a5c6ce30da92984631c72d99133076188
Chris Chou <chhschou@gmail.com>
2018-01-05 22:26:17 +1300

    update font usage to 'system'
    this will use 'san francisco' in ios, hopefully 'roboto' in android

--- native/modules/shared/styles/styleOverrides.android.js
+++ native/modules/shared/styles/styleOverrides.android.js
@@ -4,26 +4,4 @@ export default {
   container: {
     paddingTop: StatusBar.currentHeight,
   },
-  regular17: {
-    fontFamily: 'Roboto',
-    fontSize: 17,
-  },
-  regular21: {
-    fontFamily: 'Roboto',
-    fontSize: 21,
-  },
-  medium40: {
-    fontFamily: 'Roboto',
-    fontSize: 40,
-  },
-  bold16: {
-    fontFamily: 'Roboto',
-		fontWeight: 'bold',
-    fontSize: 16,
-  },
-  bold25: {
-    fontFamily: 'Roboto',
-		fontWeight: 'bold',
-    fontSize: 25,
-  },
 }
--- native/modules/shared/styles/styles.js
+++ native/modules/shared/styles/styles.js
@@ -5,6 +5,17 @@ import { mergeWith, merge } from 'ramda'
 import { Constants } from 'expo';
 import styleOverrides from './styleOverrides'
 
+/*
+{ fontWeight: '100' }, // Thin
+{ fontWeight: '200' }, // Ultra Light
+{ fontWeight: '300' }, // Light
+{ fontWeight: '400' }, // Regular
+{ fontWeight: '500' }, // Medium
+{ fontWeight: '600' }, // Semibold
+{ fontWeight: '700' }, // Bold
+{ fontWeight: '800' }, // Heavy
+{ fontWeight: '900' }, // Black 
+*/
 const baseStyles = {
 	// move to 'base' namespace if gets big
   safeArea: {
@@ -17,26 +28,32 @@ const baseStyles = {
   },
   // move to 'text' namespace if gets big
   regular17: {
-    fontFamily: 'SFUIText-Regular',
+    fontFamily: 'System',
+    fontWeight: '400',
     fontSize: 17,
   },
   regular21: {
-    fontFamily: 'SFUIText-Regular',
+    fontFamily: 'System',
+    fontWeight: '400',
     fontSize: 21,
   },
   medium40: {
-    fontFamily: 'SFUIText-Medium',
+    fontFamily: 'System',
+    fontWeight: '400',
     fontSize: 40,
   },
   bold16: {
-    fontFamily: 'SFUIText-Bold',
+    fontFamily: 'System',
+    fontWeight: '800',
     fontSize: 16,
   },
   bold25: {
-    fontFamily: 'SFUIText-Bold',
+    fontFamily: 'System',
+    fontWeight: '800',
     fontSize: 16,
   },
 }
 
 const mergedStyles = mergeWith(merge, baseStyles, styleOverrides)
+console.log('mergedstyles', JSON.stringify(mergedStyles, null, 2))
 export { mergedStyles as default }
