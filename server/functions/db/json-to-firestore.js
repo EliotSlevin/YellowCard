bc76c3a7fe1d2f91bb1ce56a93211e602e20071e
Chris Chou <chhschou@gmail.com>
2018-01-13 00:12:40 +1300

    add db seed

---
+++ server/functions/db/json-to-firestore.js
@@ -0,0 +1,28 @@
+const admin = require('../node_modules/firebase-admin');
+const serviceAccount = require("../../yc_pk.json");
+
+const data = require("./data.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://yellow-card-85ae7.firebaseio.com"
+});
+
+data && Object.keys(data).forEach(key => {
+    const nestedContent = data[key];
+
+    if (typeof nestedContent === "object") {
+        Object.keys(nestedContent).forEach(docId => {
+            admin.firestore()
+                .collection(key)
+                .doc(docId)
+                .set(nestedContent[docId])
+                .then((res) => {
+                    console.log("Document successfully written!");
+                })
+                .catch((error) => {
+                    console.error("Error writing document: ", error);
+                });
+        });
+    }
+});
