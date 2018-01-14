3bef1dd6ce276eafa7b5a42f2d81d9d4941f1829
Chris Chou <chhschou@gmail.com>
2018-01-14 16:26:33 +1300

    add unsubscribe to events

--- native/modules/nav/components/AppWithNavigationState.js
+++ native/modules/nav/components/AppWithNavigationState.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addNavigationHelpers } from 'react-navigation';
-
+import { authState$, identUpdate, logoutRequest } from '../../user'
 import AppNavigator from './AppNavigator'
 
-class App extends React.Component {
+export class App extends React.Component {
+  componentDidMount() {
+    const dispatch = this.props.dispatch
+    // subscribe to update events
+    authState$.subscribe((next) => {
+      if (!next) dispatch(logoutRequest())
+      else {
+        const user = next
+        dispatch(identUpdate(user))
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    authState$.unsubscribe()
+  }
+
   render() {
     return (
       <AppNavigator navigation={addNavigationHelpers({
