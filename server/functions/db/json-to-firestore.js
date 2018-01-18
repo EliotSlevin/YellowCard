8da88be502188df3c89a9ffcac6cc8208bb3acad
Chris Chou <chhschou@gmail.com>
2018-01-19 00:46:40 +1300

    [db] #19 - update database seed and tooling

--- server/functions/db/json-to-firestore.js
+++ server/functions/db/json-to-firestore.js
@@ -1,28 +1,50 @@
-const admin = require('../node_modules/firebase-admin');
-const serviceAccount = require("../../yc_pk.json");
+const admin = require('../node_modules/firebase-admin')
+const serviceAccount = require("../../yc_pk.json")
 
-const data = require("./seed-data.json");
+const data = require("./seed-data.json")
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://yellow-card-85ae7.firebaseio.com"
-});
+  credential: admin.credential.cert(serviceAccount),
+})
 
-data && Object.keys(data).forEach(key => {
-    const nestedContent = data[key];
-
-    if (typeof nestedContent === "object") {
-        Object.keys(nestedContent).forEach(docId => {
-            admin.firestore()
-                .collection(key)
-                .doc(docId)
-                .set(nestedContent[docId])
-                .then((res) => {
-                    console.log("Document successfully written!");
-                })
-                .catch((error) => {
-                    console.error("Error writing document: ", error);
-                });
-        });
+const setData = (key, content, docRef, batch, collections) => {
+  if (typeof content === "object") {
+    console.log('key', key)
+    if (collections.includes(key)) {
+      Object.keys(content).forEach((innerDocId) => {
+        const innerDocContent = content[innerDocId]
+        const innerDocRef = docRef.collection(key).doc(innerDocId)
+        console.log('innerdocid', innerDocId, 'innerdoccontent', innerDocContent)
+        setData(innerDocId, innerDocContent, innerDocRef, batch, collections)
+        // Object.keys(innerDocContent).forEach((innerContentInnerKey) => {
+        //   console.log('innerdocid', innerDocId, 'innerdoccontent', innerDocContent)
+        //   setData(innerContentInnerKey, innerDocContent, innerDocRef, batch, collections)
+        // })
+      })
+    } else {
+      Object.keys(content).forEach((innerKey) => {
+        const innerContent = content[innerKey]
+        console.log('innerkey', innerKey, 'innercontent', innerContent)
+        setData(innerKey, innerContent, docRef, batch, collections)
+      })
     }
-});
+  } else {
+    batch.set(docRef, { [key]: content }, { merge: true })
+  }
+}
+
+if (data) {
+  let db = admin.firestore()
+  const batch = db.batch()
+  const collections = data.collections || []
+  Object.keys(data).forEach(key => {
+    if (key != 'collections') {
+      const content = data[key]
+      setData(key, content, db, batch, collections)
+    }
+  })
+
+  batch.commit().then(() => {
+    console.log('seed applied successfully')
+  })
+}  
