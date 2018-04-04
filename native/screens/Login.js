cd74878dd11b76bbdbbccbb145ab1c68bdb98690
Elena <ebassist@live.com>
2018-04-04 16:31:07 +1000

    fixed merge

--- native/screens/Login.js
+++ native/screens/Login.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { StyleSheet, Text, TextInput, View, SafeAreaView, Image, Dimensions, TouchableOpacity ,KeyboardAvoidingView} from 'react-native';
 
-=======
-import { StyleSheet, Text, TextInput, View, SafeAreaView, Button, Image, Dimensions } from 'react-native';
->>>>>>> fc1b91d... added the login logo and text
-=======
-import { StyleSheet, Text, TextInput, View, SafeAreaView, Image, Dimensions, Button,KeyboardAvoidingView} from 'react-native';
->>>>>>> 19c4b2f... changed the form inputs and button styling
-=======
-import { StyleSheet, Text, TextInput, View, SafeAreaView, Image, Dimensions, TouchableOpacity ,KeyboardAvoidingView} from 'react-native';
->>>>>>> 8b2dd71... changed the background color and button styling
-=======
-import { StyleSheet, Text, TextInput, View, SafeAreaView, Image, Dimensions, TouchableOpacity ,KeyboardAvoidingView} from 'react-native';
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
 import { connect } from 'react-redux'
 import { FormLabel, FormInput, FormValidationMessage} from 'react-native-elements'
 
@@ -24,20 +8,8 @@ import styles from '../modules/shared/styles'
 import * as colors from '../modules/shared/colors'
 import { initFirebaseRequest } from '../modules/user'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 const width = Dimensions.get("window").width;
 
-=======
-const imageWidth = Dimensions.get("window").width / 2;
->>>>>>> fc1b91d... added the login logo and text
-=======
-const width = Dimensions.get("window").width;
->>>>>>> 19c4b2f... changed the form inputs and button styling
-=======
-const width = Dimensions.get("window").width;
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
 
 const componentStyles = StyleSheet.create({
   safeArea: {
@@ -45,11 +17,6 @@ const componentStyles = StyleSheet.create({
   },
   container: {
     ...styles.container,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
     backgroundColor: colors.YC_LOGIN_YELLOW,
     alignItems: "center"
   },
@@ -58,34 +25,7 @@ const componentStyles = StyleSheet.create({
     alignItems: "center",
     width: width / 4,
     height: width / 4
-<<<<<<< HEAD
 
-=======
-    backgroundColor: colors.YC_YELLOW,
-    alignItems: "center",
-=======
-    backgroundColor: colors.YC_LOGIN_YELLOW,
-    alignItems: "center"
->>>>>>> 8b2dd71... changed the background color and button styling
-  },
-  logo: {
-    justifyContent: "center",
-<<<<<<< HEAD
-<<<<<<< HEAD
-    width: imageWidth,
-    height: imageWidth
->>>>>>> fc1b91d... added the login logo and text
-=======
-    width: width / 2,
-    height: width / 3.5
->>>>>>> 19c4b2f... changed the form inputs and button styling
-=======
-    alignItems: "center",
-    width: width / 4,
-    height: width / 4
->>>>>>> 8b2dd71... changed the background color and button styling
-=======
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
   },
   logoText: {
     color: "#000",
@@ -93,11 +33,6 @@ const componentStyles = StyleSheet.create({
     letterSpacing: -0.5,
     marginTop: 15,
     fontWeight: "600"
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
   },
   form: {
     width: width / 1.25,
@@ -124,45 +59,7 @@ const componentStyles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "300",
     paddingVertical: 10
-<<<<<<< HEAD
-=======
->>>>>>> fc1b91d... added the login logo and text
   }
-=======
-  },
-  form: {
-    width: width / 1.25,
-    height: 40,
-    borderRadius: 5,
-    backgroundColor: "#FFF",
-    color: "#000",
-    fontSize: 0.04 * width,
-    paddingLeft: 20,
-    marginVertical: 10
-  },
-  buttonContainer: {
-    alignItems: "center"
-  },
-<<<<<<< HEAD
->>>>>>> 1cda90c... added styling
-=======
-  button: {
-    alignItems: "center",
-    backgroundColor: "#000",
-    borderRadius: 5,
-    width: width / 1.25,
-    marginVertical: 10
-  },
-  buttonText: {
-    color: "#FFF",
-    fontSize: 18,
-    fontWeight: "300",
-    paddingVertical: 10
-  }
->>>>>>> 19c4b2f... changed the form inputs and button styling
-=======
-  }
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
 });
 
 export class Login extends Component {
@@ -191,13 +88,6 @@ export class Login extends Component {
         <View style={componentStyles.container}>
           <Image resizeMode="contain" style={componentStyles.logo} source={require("./images/login.png")} />
           <Text style={componentStyles.logoText}>Yellow Card</Text>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 19c4b2f... changed the form inputs and button styling
-=======
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
           <KeyboardAvoidingView behavior="padding">
             <FormValidationMessage>{err}</FormValidationMessage>
             <FormInput inputStyle={componentStyles.form} underlineColorAndroid="transparent" keyboardType="email-address" autoCapitalize="none" placeholder="Email Address" value={this.state.email} onChangeText={text => this.setState(
@@ -206,54 +96,16 @@ export class Login extends Component {
             <FormInput inputStyle={componentStyles.form} underlineColorAndroid="transparent" secureTextEntry={true} password={true} autoCapitalize="none" placeholder="Password" onChangeText={text => this.setState(
                   { password: text }
                 )} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8b2dd71... changed the background color and button styling
-=======
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
 
             <TouchableOpacity style={componentStyles.buttonContainer} onPress={() => this.onLoginAction()}>
               <View style={componentStyles.button}>
                 <Text style={componentStyles.buttonText}>Login!</Text>
               </View>
             </TouchableOpacity>
-<<<<<<< HEAD
-<<<<<<< HEAD
           </KeyboardAvoidingView>
         </View>
       </SafeAreaView>;
   } 
-=======
-          <FormValidationMessage>{err}</FormValidationMessage>
-          <FormLabel style={componentStyles.form} >Email Address</FormLabel>
-          <FormInput keyboardType="email-address" autoCapitalize="none" placeholder="Email Address" value={this.state.email} onChangeText={text => this.setState(
-                { email: text }
-              )} />
-          <FormLabel style={componentStyles.form}>Password</FormLabel>
-          <FormInput secureTextEntry={true} password={true} autoCapitalize="none" placeholder="Password" onChangeText={text => this.setState(
-                { password: text }
-              )} />
-          <Button style={componentStyles.button} title="Login!" onPress={() => this.onLoginAction()} />
-        </View>
-      </SafeAreaView>;
-  }
->>>>>>> fc1b91d... added the login logo and text
-=======
-=======
->>>>>>> 8b2dd71... changed the background color and button styling
-          </KeyboardAvoidingView>
-        </View>
-      </SafeAreaView>;
-  } 
->>>>>>> 19c4b2f... changed the form inputs and button styling
-=======
-          </KeyboardAvoidingView>
-        </View>
-      </SafeAreaView>;
-  } 
->>>>>>> 8b2dd71cec33987718bdb4ab17bef8ee159958d2
 }
 
 const mapStateToProps = (state) => ({
